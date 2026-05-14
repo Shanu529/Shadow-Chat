1c0956c904a7831217ceeda3c8863210f5edf591
Shanu Chhetri <shnau.chhetri529@gmail.com>
2026-05-14 15:45:36 +0530

    refactor(Navbar): update logout functionality and improve UI elements chore(deps): upgrade Next.js and add react-router-dom dependency

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -3,9 +3,21 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+// import { useNavigate } from "react-router-dom";
+import { useUser } from "../context/UsersContext";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+
+  // const navigate = useNavigate();
+
+  const {  logout  } = useUser();
+
+  const logoutHandler = ()=>{
+    logout()
+    // navigate("/")
+  }
+  
 
   return (
     <>
@@ -25,15 +37,18 @@ export default function Navbar() {
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
             <Link href="/privacy">Privacy</Link>
+            <Link href="/">Start chat</Link>
+           
+
+          </div>
+          <div
+          onClick={()=>logout()}
+            className="hidden md:block cursor-pointer bg-blue-400 px-4 py-1.5 rounded-full text-xs font-semibold text-white"
+          >
+            Logout
           </div>
 
-          {/* Desktop Button */}
-          <Link
-            href="/"
-            className="hidden md:block bg-gradient-to-r from-blue-500 to-green-400 px-4 py-1.5 rounded-full text-xs font-semibold text-white"
-          >
-            Start Chat
-          </Link>
+          
 
           {/* Mobile Button */}
           <button
@@ -81,10 +96,17 @@ export default function Navbar() {
           <Link
             href="/"
             onClick={() => setOpen(false)}
-            className="mt-4 bg-gradient-to-r from-blue-500 to-green-400 px-4 py-2 rounded-full text-xs font-semibold text-white text-center"
+            className="mt-4 bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-full text-xs font-semibold text-white text-center"
           >
             Start Chat
           </Link>
+
+          <div
+            onClick={() => logout()}
+            className="bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-full text-xs font-semibold text-white text-center"
+          >
+            Logout
+          </div>
         </div>
       </div>
     </>
